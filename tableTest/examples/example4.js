daa5cfa6a1694f0a202bc9c8f0edba2ca8a8d5ad
Christopher Rieder <christopher.rieder@gmail.com>
2019-02-08 00:04:30 -0300

    small high order function optimizations

--- tableTest/examples/example4.js
+++ tableTest/examples/example4.js
@@ -137,7 +137,6 @@ function comparer (a, b) {
 // they don't need to be defined here
 let columnFilters = {};
 
-// TODO: implement functions
 // these filter functions will be called according to each column definition
 // some functions just return a function without doing anything else
 // all these function are high order functions for consistency, even when it's not necessary
@@ -146,6 +145,7 @@ let filterFunctions = {
   anchoredTextFiltering: function (boolBeggining, boolEnd) {
     const anchorBeggining = boolBeggining ? '^' : '';
     const anchorEnd = boolEnd ? '$' : '';
+    // a filter anchored at the start and end will match the exact word
 
     return function (text, filter) {
       let regex = new RegExp(anchorBeggining + filter + anchorEnd, 'i');
@@ -155,45 +155,55 @@ let filterFunctions = {
 
   wordFiltering: function (boolSplit) {
     // check split boolean for spliting words or not.
-    return function (text, filter) {
-      if (boolSplit) {
+    // then it will try to match words in separate regex
+    // and the text has to pass all the filters (it's a logical AND)
+    // this would be a prime candidate for a better search algorithm
+    if (boolSplit) {
+      return function (text, filter) {
         return filter.split(' ')
           .map(word => new RegExp(word, 'i'))
           .every(regex => regex.test(text)); // 'be je' will match 'bean jelly', but not 'bean salad'
-      } else {
+      };
+    } else {
+      return function (text, filter) {
         let regex = new RegExp(filter, 'i');
         return regex.test(text);
-      }
-    };
+      };
+    }
   },
 
   numberRangeFiltering: function (boolRange) {
-    return function (numberStr, filter) {
-      if (boolRange) {
+    if (boolRange) {
+      return function (numberStr, filter) {
         let escapedFilter = filter.toString().match(/[\d\-+\s]*/g).join('');
         let filterNumber = escapedFilter.toString().match(/\d*/g).join('');
 
+        // this tests for '+300' or '300+'
         if (/^\+/.test(escapedFilter) || /\+$/.test(escapedFilter)) {
           return numberStr >= filterNumber;
         }
 
+        // this tests for '-300' or '300-'
         if (/^-/.test(escapedFilter) || /-$/.test(escapedFilter)) {
           return numberStr <= filterNumber;
         }
 
+        // this tests for '300-500' or '300 500'
         if (/^\d+[\s-]\d+$/.test(escapedFilter)) {
           let regex = /\d+/g;
           let filterMin = Math.min(...escapedFilter.match(regex));
           let filterMax = Math.max(...escapedFilter.match(regex));
           return numberStr <= filterMax && numberStr >= filterMin;
         }
-      } else {
+      };
+    } else {
+      return function (numberStr, filter) {
         let escapedFilter = filter.toString().match(/\d*/g).join('');
         let regex = new RegExp('^' + escapedFilter + '$', 'i');
         return regex.test(numberStr);
-      }
-    };
-  }, // +300, -300, 300+, 300-, 300-400, 300 400
+      };
+    }
+  },
 
   booleanFiltering: function () {
     return function (boolValue, filter) {
@@ -206,7 +216,7 @@ let filterFunctions = {
     return function () {
       return true;
     };
-  } // use a library or package. moment.js?
+  } // TODO: use a library or package. moment.js?
 };
 
 let columnFilterFunctions = {
