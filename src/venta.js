c6065b1f92ad078f751f23a60075edcd245a7003
Christopher Rieder <christopher.rieder@gmail.com>
2019-02-09 16:45:45 -0300

    starting venta section

---
+++ src/venta.js
@@ -0,0 +1,118 @@
+// VARIABLE DEFINITIONS FOR SLICKGRID TABLE
+let dataView;
+let grid;
+let data = [];
+
+let options = {
+  enableCellNavigation: true,
+  multiSelect: false,
+  topPanelHeight: 30,
+  rowHeight: 30,
+  editable: true,
+  autoEdit:true,
+  explicitInitialization: true
+};
+
+// TODO: read column preferences from a configuration file, persist this preferences
+// COLUMNS DEFINITIONS
+let columns = [
+  {
+    id: 'CANTIDAD',
+    name: 'cantidad',
+    field: 'CANTIDAD',
+    minWidth: 60,
+    editor: Slick.Editors.Text,
+    cssClass: 'cell-title'
+  },
+  {
+    id: 'CODIGO',
+    name: 'codigo',
+    field: 'CODIGO',
+    minWidth: 120,
+    cssClass: 'cell-title'
+  },
+  {
+    id: 'DESCRIPCION',
+    name: 'Descripcion',
+    field: 'DESCRIPCION',
+    minWidth: 300,
+    cssClass: 'cell-title'
+  },
+  {
+    id: 'PRECIO_UNITARIO',
+    name: '$ Unit',
+    field: 'PRECIO_UNITARIO',
+    minWidth: 80,
+    cssClass: 'cell-title'
+  },
+  {
+    id: 'PRECIO_TOTAL',
+    name: '$ Total',
+    field: 'PRECIO_TOTAL',
+    minWidth: 80,
+    cssClass: 'cell-title'
+  }
+];
+
+dataView = new Slick.Data.DataView();
+grid = new Slick.Grid('#myGrid', dataView, columns, options);
+grid.setSelectionModel(new Slick.RowSelectionModel());
+
+// wire up model events to drive the grid
+// !! both dataView.onRowCountChanged and dataView.onRowsChanged MUST be wired to correctly update the grid
+// see Issue#91
+dataView.onRowCountChanged.subscribe(function (e, args) {
+  grid.updateRowCount();
+  grid.render();
+});
+
+dataView.onRowsChanged.subscribe(function (e, args) {
+  grid.invalidateRows(args.rows);
+  grid.render();
+});
+
+grid.onCellChange.subscribe(function(event,activeCell) {
+  let {cell, row, item} = activeCell;
+  item.PRECIO_TOTAL = item.PRECIO_UNITARIO * item.CANTIDAD;
+  grid.invalidateRow(row);
+  grid.render()
+})
+
+grid.init();
+// initialize the model after all the events have been hooked up
+$('#gridContainer').resizable();
+
+
+setTimeout(e=>addVentaItem('ZH2932030828'),100)
+setTimeout(e=>addVentaItem('5985561826014'),200)
+setTimeout(e=>addVentaItem('4883803077006'),300)
+setTimeout(e=>addVentaItem('4883803077006'),1000)
+
+// TODO: separate fetching data from intializing the grid
+async function getArticuloByCodigo (codigo) {
+  const res = await axios(`http://192.168.0.2:3000/api/articulo/${codigo}`);
+  return res.data[0];
+}
+
+async function addVentaItem (codigo) {
+  let idx = data.findIndex(e => e.CODIGO === codigo);
+  let articulo;
+
+  if (idx !== -1) {
+    articulo = data[idx];
+    articulo.CANTIDAD += 1;
+    articulo.PRECIO_UNITARIO = articulo.PRECIO_LISTA;
+    articulo.PRECIO_TOTAL = articulo.PRECIO_UNITARIO * articulo.CANTIDAD;
+    grid.invalidateRow(dataView.getIdxById(articulo.id));
+    grid.render()
+  } else {
+    articulo = await getArticuloByCodigo(codigo);
+    articulo.CANTIDAD = 1;
+    articulo.PRECIO_UNITARIO = articulo.PRECIO_LISTA;
+    articulo.PRECIO_TOTAL = articulo.PRECIO_UNITARIO * articulo.CANTIDAD;
+    data.push(articulo);
+    dataView.beginUpdate();
+    dataView.setItems(data);
+    dataView.endUpdate();
+  }
+}
