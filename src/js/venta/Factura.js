b8c32f74af045c4abe2aee0d2999b91cd24143ff
Christopher Rieder <christopher.rieder@gmail.com>
2019-02-16 01:48:51 -0300

    more vanilljs stuff

--- src/js/venta/Factura.js
+++ src/js/venta/Factura.js
@@ -1,38 +1,32 @@
-import * as databaseRead from '../database/getData';
-import * as databaseWrite from '../database/writeData';
 import {format as dateFormat} from 'date-fns';
-// import {Input, InputText} from '../components/inputs';
-import {errorShakeEffect} from '../components/effects';
-import {condicionesPago, descuentoMax} from '../constants/bussinessConstants';
-import {facturaDOM, clienteDOM, vendedorDOM, descuentoDOM, turnoDOM, condicionPagoDOM} from '../utilities/selectors';
+import {facturaDOM, clienteDOM, vendedorDOM, descuentoDOM, turnoDOM} from '../utilities/selectors';
 
 export default class Factura {
-  constructor () {
-    this._numeroFactura = 0;
-    this._cliente = {};
-    this._vendedor = {};
+  constructor (numeroFactura, cliente, vendedor, turno) {
+    this._numeroFactura = numeroFactura;
+    Object.defineProperty(this, '_numeroFactura', {writable: false});
+    facturaDOM.value = numeroFactura; // Freeze numeroFactura
+
+    this.cliente = cliente;
+    this.vendedor = vendedor;
     this._fecha = null;
     this._descuento = 0;
-    this._turno = {};
+    this._turno = turno;
     this._anulada = false;
     this._condicionDePago = 'EFECTIVO';
     this.itemsFactura = [];
   }
 
   get numeroFactura () { return parseInt(this._numeroFactura); }
-  get cliente () { return parseInt(this._cliente); }
-  get vendedor () { return parseInt(this._vendedor); }
+  get cliente () { return this._cliente; }
+  get vendedor () { return this._vendedor; }
   get fechaStr () { return dateFormat(this._fecha, 'MM/dd/yyyy'); }
   get descuento () { return parseFloat(this._descuento); }
   get turno () { return this._turno; }
   get anulada () { return this._anulada; }
   get anuladaSQLite () { return this._anulada ? 1 : 0; }
   get condicionDePago () { return this._condicionDePago; }
-
-  set numeroFactura (value) {
-    this._numeroFactura = value;
-    facturaDOM.value = value;
-  }
+  get precioTotal () { return this.itemsFactura.reduce((monto, item) => monto + item.precioTotal, 0); }
 
   set cliente (obj) {
     this._cliente = obj;
@@ -64,9 +58,8 @@ export default class Factura {
     this._anulada = bool;
   }
 
-  toJsonString () {
+  toServerJsonAPI () {
     this._fecha = new Date(); // SEND WITH UPDATED DATE_TIME
-    Object.freeze(this);
     return {
       NUMERO_FACTURA: this.numeroFactura,
       FECHA_HORA: this.fechaStr, // UNIX EPOCH TIME
@@ -77,24 +70,23 @@ export default class Factura {
     };
   }
 
-  async addItem (codigo) {
-    if (!codigo) return false;
-    const articulo = this.itemsFactura.find(item => item.CODIGO === codigo);
+  async addItem (articuloData) {
+    const articulo = this.itemsFactura.find(item => item.id === articuloData.id);
     if (articulo) {
       articulo.cantidad += 1;
-      return true;
     } else { // add new articulo
-      const articuloData = await databaseRead.getArticuloByCodigo(codigo);
-      if (!articuloData) return false;
       const articulo = new ItemFactura(articuloData);
-      // FIXME: ADD AND PASE DOMNODE?
       this.itemsFactura.push(articulo);
-      return true;
     }
+
+    // TODO: MAKE DOM NODES. PASS IN CONSTRUCTOR?
   }
 
-  deleteItem () {
-    // TODO: IMPLEMENT
+  deleteItem (id) {
+    const articulo = this.itemsFactura.find(item => item.id === id);
+    this.itemsFactura = this.itemsFactura.filter(item => item.id === id);
+
+    // TODO: DESTROY DOM NODES.
   }
 }
 
@@ -110,11 +102,17 @@ class ItemFactura {
     this._marca = articulo.MARCA;
     this._cantidad = 1;
     this._descuentoIndividual = articulo.PROMO_BOOL ? articulo.DESCUENTO_PROMO : 0;
-    this._precioUnitario = this.updatePrice();
   }
 
   updatePrice (descuento = 0, condicionDePago = 'EFECTIVO') {
     const tipoPrecio = condicionDePago === 'EFECTIVO' ? 'precioContado' : 'precioLista';
     this._precioUnitario = this[tipoPrecio] * (1 - descuento / 100) * 100 * (1 - this.descuentoIndividual / 100) * 100;
+  }
+
+  get montoTotal () {
+    return this._precioUnitario * this._cantidad;
+  }
+
+  toServerJsonAPI () {
   }
 }
--- src/js/venta/venta.js
+++ src/js/venta/venta.js
@@ -14,12 +14,13 @@ let audioOk = require('../../resources/audio/ok.wav');
 
 initialLoad();
 
-window.factura = new Factura();
-
 async function initialLoad () {
-  window.factura.numeroFactura = await databaseRead.getNewNumeroFactura();
-  window.factura.cliente = await databaseRead.getClienteById(1);
-  window.factura.vendedor = await databaseRead.getVendedorById(1);
+  const numeroFactura = await databaseRead.getNewNumeroFactura();
+  const cliente = await databaseRead.getClienteById(1);
+  const vendedor = await databaseRead.getVendedorById(1);
+
+  window.factura = new Factura(numeroFactura, cliente, vendedor);
+
   Object.keys(condicionesPago).forEach(condicion => {
     var option = document.createElement('option');
     option.value = condicion;
@@ -47,6 +48,11 @@ descuentoDOM.addEventListener('input', event => {
   if (!/\d+[.0]+$/.test(event.target.value)) {
     event.target.value = parseFloat(event.target.value) || '0';
   }
+  if (parseFloat(event.target.value) > descuentoMax) {
+    event.target.value = descuentoMax;
+    errorShakeEffect(event.target);
+    // Toast(descuentoMax es el limite maximo);
+  }
   window.factura.descuento = event.target.value;
 });
 
@@ -54,103 +60,49 @@ condicionPagoDOM.addEventListener('change', event => {
   window.factura.condicionDePago = event.target.value;
 });
 
-// class Venta extends Component {
-//   constructor (props) {
-//     super(props);
-//     this.state = {
-//       dataset: [],
-//       codigo: '',
-//       condicionPago: 'EFECTIVO',
-//     };
-//   }
+async function onSubmit (event) {
+  event.preventDefault();
+  // TODO: POST DATA TO API AFTER CONFIRMATION AND VALIDATION
+  // TODO: VALIDATIONS
+  // TODO: make confirmation stage
+  // TODO: inform user
+  // TODO: CALL SUBMISSIONS
 
-//   handleDescuento (event) {
-//     let descuento = event.target.value;
-//     this.setState(prevState => {
-//       if (/^[1-9]\d*\.$/.test(descuento)) {
-//         return {descuento}; // Allow ending in dot without processing, for floats numbers.
-//       } else if (/^[1-9]\d*\.?\d*$/.test(descuento) || descuento === '') { // valid positive float
-//         descuento = parseFloat(descuento) || '';
-//         if (descuento > descuentoMax) {
-//           descuento = descuentoMax;
-//           errorShakeEffect(this.descuentoInput.current);
-//           // Toast(descuentoMax es el limite maximo);
-//         }
-//         this.updateAllPrices(descuento, this.state.condicionPago);
-//         return {descuento};
-//       } else {
-//         errorShakeEffect(this.descuentoInput.current);
-//         this.updateAllPrices(prevState.descuento, this.state.condicionPago);
-//         return prevState;
-//       }
-//     });
-//   }
+  let facturaJson = window.factura.toServerJsonAPI();
+  Object.freeze(window.factura);
+  const facturaId = await databaseWrite.postFactura(facturaJson);
+  window.factura.itemsFactura.forEach(item => {
+    databaseWrite.postItemFactura(item.toServerJsonAPI());
+  });
 
-//   handleCondicionPago (event) {
-//     this.setState({condicionPago: event.target.value});
-//     this.updateAllPrices(this.state.descuento, event.target.value);
-//   }
+  const monto = window.factura.montoTotal;
 
-//   async onSubmit (event) {
-//     event.preventDefault();
-//     // TODO: POST DATA TO API AFTER CONFIRMATION AND VALIDATION
-//     // TODO: VALIDATIONS
-//     // TODO: make confirmation stage
-//     // TODO: inform user
-//     // TODO: CALL SUBMISSIONS
+  if (this.state.condicionPago === 'EFECTIVO') {
+    const pago = {
+      FACTURA_ID: facturaId,
+      MONTO: monto,
+      TIPO_PAGO_ID: condicionesPago[this.state.condicionPago],
+      ESTADO: 'PAGADO'
+    };
+    const pagoId = await databaseWrite.postObjectToAPI(pago, 'pago');
+    console.log(pagoId);
+  } else {
+    const pago = {
+      FACTURA_ID: facturaId,
+      MONTO: monto,
+      TIPO_PAGO_ID: condicionesPago[this.state.condicionPago],
+      ESTADO: 'PENDIENTE'
+    };
+    const pagoId = await databaseWrite.postObjectToAPI(pago, 'pago');
+    console.log(pagoId);
+  }
 
-//     let descuento = parseFloat(this.state.descuento) || 0;
+  // TODO: actualizacion de STOCK luego de que toda la transaccion fue exitosa.
 
-//     let factura = {
-//       NUMERO_FACTURA: this.state.currentNroFactura,
-//       FECHA_HORA: new Date().getTime(), // UNIX EPOCH TIME
-//       DESCUENTO: descuento,
-//       CLIENTE_ID: this.state.cliente.id,
-//       TURNO_ID: 0, // TODO: MAKE TURNO
-//       ANULADA: 0 // FIXME: SQLITE BOOLEANS... NOT EXISTENT
-//     };
-
-//     const facturaId = await databaseWrite.postFactura(factura);
-
-//     this.state.dataset.forEach(({id, CANTIDAD, PRECIO_UNITARIO, DESCUENTO}) => {
-//       let item = {CANTIDAD, PRECIO_UNITARIO, DESCUENTO};
-//       item.ARTICULO_ID = id;
-//       item.FACTURA_ID = facturaId;
-//       item.DESCUENTO = DESCUENTO || 0;
-//       databaseWrite.postItemFactura(item);
-//     });
-
-//     const monto = this.state.dataset.reduce((monto, item) => monto + item.PRECIO_UNITARIO * item.CANTIDAD, 0);
-
-//     if (this.state.condicionPago === 'EFECTIVO') {
-//       const pago = {
-//         FACTURA_ID: facturaId,
-//         MONTO: monto,
-//         TIPO_PAGO_ID: condicionesPago[this.state.condicionPago],
-//         ESTADO: 'PAGADO'
-//       };
-//       const pagoId = await databaseWrite.postObjectToAPI(pago, 'pago');
-//       console.log(pagoId);
-//     } else {
-//       const pago = {
-//         FACTURA_ID: facturaId,
-//         MONTO: monto,
-//         TIPO_PAGO_ID: condicionesPago[this.state.condicionPago],
-//         ESTADO: 'PENDIENTE'
-//       };
-//       const pagoId = await databaseWrite.postObjectToAPI(pago, 'pago');
-//       console.log(pagoId);
-//     }
-
-//     // TODO: actualizacion de STOCK luego de que toda la transaccion fue exitosa.
-
-//     // TODO: AFTER FINISHING REFRESH CURRENT NUMERO FACTURA, REFRESH INTERFACE
-//   }
+  // TODO: AFTER FINISHING REFRESH CURRENT NUMERO FACTURA, REFRESH INTERFACE
+}
 
 //   componentDidMount () {
-//     databaseRead.getNewNumeroFactura().then((currentNroFactura) => this.setState({currentNroFactura}));
-//     databaseRead.getClienteById(1).then((cliente) => this.setState({cliente})); // FIXME: DA WARNING EN REACT. PORQUE?
-//     databaseRead.getVendedorById(1).then((vendedor) => this.setState({vendedor})); // FIXME: DA WARNING EN REACT. PORQUE?
 //     // TODO: get turno
 //     this.codigoInput.current.focus();
 //     setTimeout(e => this.addVentaItem('ZH2932030828'), 100);
@@ -159,66 +111,26 @@ condicionPagoDOM.addEventListener('change', event => {
 //     setTimeout(e => this.addVentaItem('4883803077006'), 3000);
 //   }
 
-//   getDate () {
-//     return dateFormat(new Date(), 'MM/dd/yyyy');
-//   }
+async function addItem (event) { // ZH2932030828
+  let codigo = codigoDOM.value;
+  if (!codigo) return false;
+  let articuloData = await databaseRead.getArticuloByCodigo(codigo);
+  // if found in database, add, there can be no error after this;
+  // else display error, codigo not found or database error.
+  if (articuloData) {
+    // TODO: USER POSITIVE FEEDBACK. TOAST?
+    window.factura.addItem(articuloData);
+    var aud = new window.Audio(audioOk);
+    aud.play();
+  } else {
+    errorShakeEffect(this.codigoInput.current);
+    var aud2 = new window.Audio(audioError);
+    aud2.play();
+  }
 
-//   async addItem (event) { // ZH2932030828
-//     let boolAdded = await this.addVentaItem(this.state.codigo);
-//     this.setState({codigo: ''});
-//     this.codigoInput.current.focus();
-//     if (boolAdded) {
-//       // TODO: USER POSITIVE FEEDBACK. TOAST?
-//       var aud = new window.Audio(audioOk);
-//       aud.play();
-//     } else {
-//       errorShakeEffect(this.codigoInput.current);
-//       var aud2 = new window.Audio(audioError);
-//       aud2.play();
-//     }
-//   }
+  codigoDOM.focus();
+}
 
-//   async addVentaItem (codigo) {
-//     let articulo = this.state.dataset.find(e => e.CODIGO === codigo);
-
-//     if (articulo) {
-//       articulo.CANTIDAD += 1;
-//     } else {
-//       if (!codigo) return false;
-//       articulo = await databaseRead.getArticuloByCodigo(codigo);
-//       if (!articulo) return false;
-//       articulo.CANTIDAD = 1;
-//       let dataset = [...this.state.dataset, articulo];
-//       this.setState({dataset});
-//     }
-
-//     this.updateArticulo(articulo, this.state.descuento, this.state.condicionPago);
-//     this.setState({});
-//     return true;
-//   }
-
-//   updateArticulo (articulo, descuento = 0, condicion = 'EFECTIVO') {
-//     const tipoPrecio = condicion === 'EFECTIVO' ? 'PRECIO_CONTADO' : 'PRECIO_LISTA';
-
-//     articulo.PRECIO_UNITARIO = articulo[tipoPrecio] * (100 - descuento) / 100;
-//     if (articulo.DESCUENTO) { // descuento individual del item
-//       articulo.PRECIO_UNITARIO *= (100 - articulo.DESCUENTO) / 100;
-//     }
-//     articulo.PRECIO_TOTAL = articulo.PRECIO_UNITARIO * articulo.CANTIDAD;
-//   }
-
-//   updateAllPrices (descuento, condicion = 'EFECTIVO') {
-//     const tipoPrecio = condicion === 'EFECTIVO' ? 'PRECIO_CONTADO' : 'PRECIO_LISTA';
-//     this.state.dataset.forEach(articulo => {
-//       articulo.PRECIO_UNITARIO = articulo[tipoPrecio] * (100 - descuento) / 100;
-//       if (articulo.DESCUENTO) { // descuento individual del item
-//         articulo.PRECIO_UNITARIO = articulo.PRECIO_UNITARIO * articulo.DESCUENTO;
-//       }
-//       articulo.PRECIO_TOTAL = articulo.PRECIO_UNITARIO * articulo.CANTIDAD;
-//     });
-//   }
-
-//   /*
 // async function selectClient (nro) {
 //   // TODO: SELECT A DIFFERENT CLIENT
 // }
@@ -236,79 +148,3 @@ condicionPagoDOM.addEventListener('change', event => {
 //   // TODO: handle the selection and search of articulos
 // }
 //   */
-
-//   getVentaTable () {
-//     return this.state.dataset.map(row => {
-//       return (
-//         <tr key={row.id}>
-//           <td>{row.CANTIDAD}</td>
-//           <td>{row.CODIGO}</td>
-//           <td>{row.DESCRIPCION}</td>
-//           <td>{row.PRECIO_UNITARIO}</td>
-//           <td>{row.PRECIO_TOTAL}</td>
-//           <td>{row.DESCUENTO}</td>
-//         </tr>
-//       );
-//     });
-//   }
-
-//   render () {
-//     return (
-//       <div>
-//         <form onSubmit={this.onSubmit} className='venta' id='venta'>
-//           <div className='panel'>
-//             <Input context='venta' disabled tipo='factura' value={this.state.currentNroFactura} />
-//             <Input context='venta' tipo='cliente' value={this.state.cliente.NOMBRE} />
-//           </div>
-//           <div className='panel'>
-//             <div>
-//               <label htmlFor='venta-condicion-de-pago'>Condicion de pago</label>
-//               <select value={this.state.condicionPago} name='venta-condicion-de-pago' id='venta-condicion-de-pago' onChange={this.handleCondicionPago}>
-//                 {Object.keys(condicionesPago).map((condicion, i) => <option key={i} value={condicion}>{condicion}</option>)}
-//               </select>
-//             </div>
-//             <InputText context='venta' tipo='descuento' value={this.state.descuento} onChange={this.handleDescuento} ref={this.descuentoInput} />
-//           </div>
-//           <div className='panel'>
-//             <InputText context='venta' tipo='codigo' value={this.state.codigo} onChange={this.handleCodigo} ref={this.codigoInput} />
-//             <button className='codigo-search' onClick={this.addItem}>BUTTON</button>
-//           </div>
-//           <div className='panel'>
-//             <Input context='venta' tipo='observaciones' value={this.state.observaciones} onChange={this.handleObservaciones} />
-//           </div>
-//           <table id='table'>
-//             <thead>
-//               <tr>
-//                 <th>Cantidad</th>
-//                 <th>Codigo</th>
-//                 <th>Descripcion</th>
-//                 <th>Precio Unitario</th>
-//                 <th>Precio Total</th>
-//                 <th>Descuento</th>
-//               </tr>
-//             </thead>
-//             <tbody>
-//               {this.getVentaTable()}
-//             </tbody>
-//           </table>
-//           <div className='panel'>
-//             <Input context='venta' disabled tipo='vendedor' value={this.state.vendedor.NOMBRE} />
-//             <Input context='venta' disabled tipo='turno' value={this.state.turno} />
-//             <Input context='venta' disabled tipo='fecha' value={this.getDate()} />
-//           </div>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
-
-// ReactDOM.render(<Venta ref={(ventaApp) => { window.ventaApp = ventaApp; }} />, document.getElementById('app'));
-
-// id: 'CANTIDAD',
-// id: 'CODIGO',
-// id: 'DESCRIPCION',
-// id: 'PRECIO_UNITARIO',
-// id: 'PRECIO_TOTAL',
-// id: 'DESCUENTO',
-
-// updateArticulo(activeCell.item, window.ventaApp.state.descuento, window.ventaApp.state.condicionPago);
