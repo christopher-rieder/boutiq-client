c4a81a00b437c357d70eaec88e2d4eb76a8dabc9
Christopher Rieder <christopher.rieder@gmail.com>
2019-02-17 10:37:03 -0300

    more simplification

--- src/js/articulo/crudArticulo.js
+++ src/js/articulo/crudArticulo.js
@@ -1,94 +1,100 @@
 import * as databaseRead from '../database/getData';
 import * as databaseWrite from '../database/writeData';
 
-const selectorsDOM = {};
+const selectorsDOM = {
+  id: document.querySelector('#crud-articulo__id'),
+  CODIGO: document.querySelector('#crud-articulo__CODIGO'),
+  DESCRIPCION: document.querySelector('#crud-articulo__DESCRIPCION'),
+  PRECIO_LISTA: document.querySelector('#crud-articulo__PRECIO_LISTA'),
+  PRECIO_CONTADO: document.querySelector('#crud-articulo__PRECIO_CONTADO'),
+  PRECIO_COSTO: document.querySelector('#crud-articulo__PRECIO_COSTO'),
+  STOCK: document.querySelector('#crud-articulo__STOCK'),
+  RUBRO_ID: document.querySelector('#crud-articulo__RUBRO_ID'),
+  MARCA_ID: document.querySelector('#crud-articulo__MARCA_ID'),
+  PROMO_BOOL: document.querySelector('#crud-articulo__PROMO_BOOL'),
+  DESCUENTO_PROMO: document.querySelector('#crud-articulo__DESCUENTO_PROMO')
+};
 
-async function loadData () {
-  let articulo = await databaseRead.getArticuloById(68);
+window.addEventListener('load', async event => {
+  loadTables();
+  const url = new URL(document.URL);
+  const searchParams = new URLSearchParams(url.search);
+  const cod = searchParams.get('codigo');
+  let articulo = await databaseRead.getArticuloByCodigo(cod);
+  if (articulo) {
+    loadData(articulo);
+  }
+});
+
+document.querySelector('#search-codigo').addEventListener('search', async event => {
+  let articulo = await databaseRead.getArticuloByCodigo(event.target.value);
+  if (articulo) {
+    loadData(articulo);
+  }
+  event.target.value = '';
+});
+
+async function loadData (articulo) {
+  // get support tables
+  const supportTables = await loadTables();
 
   // SET INPUTS WITH THE DATA LOADED FROM THE DATABASE
-  // AND ADD LISTENERS TO REFLECT THE STATE OF THE VIEW IN THE DATA
   Object.keys(articulo).forEach(prop => {
-    selectorsDOM[prop] = document.querySelector('#crud-articulo__' + prop);
-    if (selectorsDOM[prop].tagName === 'INPUT' && selectorsDOM[prop].type !== 'checkbox') {
+    if (selectorsDOM[prop].nodeName === 'INPUT' && selectorsDOM[prop].type !== 'checkbox') {
       selectorsDOM[prop].value = articulo[prop];
-      selectorsDOM[prop].addEventListener('input', event => {
-        articulo[prop] = event.target.value;
-      });
     }
-    if (selectorsDOM[prop].tagName === 'INPUT' && selectorsDOM[prop].type === 'checkbox') {
+    if (selectorsDOM[prop].nodeName === 'INPUT' && selectorsDOM[prop].type === 'checkbox') {
       selectorsDOM[prop].checked = !!(articulo[prop]);
-      selectorsDOM[prop].addEventListener('change', event => {
-        articulo[prop] = !!(event.target.checked);
-      });
+    }
+    if (selectorsDOM[prop].nodeName === 'SELECT') {
+      const idx = supportTables[prop].findIndex(el => el.id === articulo[prop]);
+      selectorsDOM[prop].selectedIndex = idx;
     }
   });
 
-  const marcas = await loadMarcas(articulo);
-  const rubros = await loadRubros(articulo);
+  setListeners(articulo, supportTables);
+}
 
-  window.addEventListener('dblclick', event => {
-    if (event.ctrlKey && event.shiftKey) {
-      putArticulo();
-    }
+async function loadTables () {
+  const tables = {
+    MARCA_ID: await databaseRead.getTable('marca'),
+    RUBRO_ID: await databaseRead.getTable('rubro')
+  };
+
+  Object.keys(tables).forEach(key => {
+    tables[key].forEach(element => {
+      let option = document.createElement('option');
+      option.value = element.id;
+      option.innerHTML = element.NOMBRE;
+      selectorsDOM[key].appendChild(option);
+    });
   });
 
+  return tables;
+}
+
+function setListeners (articulo, supportTables) {
+  // MAIN LISTENER
+  // IT DEPENDS OF CORRECTLY NAMING THE IDS OF HTML ELEMENTS
   window.addEventListener('input', event => {
-    console.log('Event Captured From window', event);
-  });
-
-  async function putArticulo () {
-    databaseWrite.putObjectToAPI(articulo, 'articulo');
-  }
-}
-
-async function loadMarcas (articulo) {
-  let marcas = await databaseRead.getTable('marca');
-  var indexMarca = 0;
-  marcas.forEach((marca, i) => {
-    var option = document.createElement('option');
-    option.dataset.id = marca.id;
-    option.value = marca.NOMBRE;
-    option.innerHTML = marca.NOMBRE;
-    if (marca.id === articulo.MARCA_ID) {
-      indexMarca = i;
-      articulo.MARCA_ID = marca.id;
+    const property = event.target.id.replace('crud-articulo__', '');
+    if (event.target.type === 'checkbox') {
+      articulo[property] = !!(event.target.checked);
+      return true;
     }
-    selectorsDOM.MARCA_ID.appendChild(option);
-  });
-
-  selectorsDOM.MARCA_ID.selectedIndex = indexMarca;
-  selectorsDOM.MARCA_ID.addEventListener('change', event => {
-    const id = document.querySelector('#crud-articulo__MARCA_ID').selectedOptions[0].dataset.id;
-    articulo.MARCA_ID = marcas.filter(marca => marca.id === parseInt(id))[0].id;
-  });
-
-  return marcas;
-}
-
-async function loadRubros (articulo) {
-  let rubros = await databaseRead.getTable('rubro');
-  var indexRubro = 0;
-
-  rubros.forEach((rubro, i) => {
-    var option = document.createElement('option');
-    option.dataset.id = rubro.id;
-    option.value = rubro.NOMBRE;
-    option.innerHTML = rubro.NOMBRE;
-    if (rubro.id === articulo.RUBRO_ID) {
-      indexRubro = i;
-      articulo.RUBRO_ID = rubro.id;
+    if (event.target.nodeName === 'SELECT') {
+      const id = event.target.selectedOptions[0].value;
+      articulo[property] = supportTables[property].filter(marca => marca.id === parseInt(id))[0].id;
+      return true;
     }
-    selectorsDOM.RUBRO_ID.appendChild(option);
+    if (event.target.nodeName === 'INPUT') {
+      articulo[property] = event.target.value;
+    }
   });
 
-  selectorsDOM.RUBRO_ID.selectedIndex = indexRubro;
-  selectorsDOM.RUBRO_ID.addEventListener('change', event => {
-    const id = document.querySelector('#crud-articulo__RUBRO_ID').selectedOptions[0].dataset.id;
-    articulo.RUBRO_ID = rubros.filter(rubro => rubro.id === parseInt(id))[0].id;
+  document.querySelector('.btn-guardar').addEventListener('click', event => {
+    console.log(articulo);
+    // databaseWrite.putObjectToAPI(articulo, 'articulo');
+    window.location.reload(true);
   });
-
-  return rubros;
 }
-
-loadData();
