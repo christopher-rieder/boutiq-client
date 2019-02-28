2ca0c23d18133109ab4fa12b60c12ea3cf2fa4ca
Christopher Rieder <christopher.rieder@gmail.com>
2019-02-28 00:27:00 -0300

    removing async in function signature in getData.js

--- src/js/database/getData.js
+++ src/js/database/getData.js
@@ -1,41 +1,46 @@
 // const URL = 'http://181.167.238.144:3000';
 const URL = 'http://192.168.0.2:3000';
 
-async function getTable (table) {
+function getTable (table) {
   return fetch(`${URL}/api/rawTables/${table}`).then(res => res.json());
 }
 
-async function getAllArticulos () {
+function getAllArticulos () {
   return fetch(`${URL}/api/rawTables/full_articulos`).then(res => res.json());
 }
 
-async function getArticuloByCodigo (codigo) {
+function getAllFacturas () {
+  return fetch(`${URL}/api/factura/all`).then(res => res.json());
+}
+
+function getArticuloByCodigo (codigo) {
   return fetch(`${URL}/api/articulo/codigo/${codigo}`).then(res => res.json());
 }
 
-async function getArticuloById (id) {
+function getArticuloById (id) {
   return fetch(`${URL}/api/articulo/id/${id}`).then(res => res.json());
 }
 
-async function getClienteById (id) {
+function getClienteById (id) {
   return fetch(`${URL}/api/cliente/${id}`).then(res => res.json());
 }
 
-async function getVendedorById (id) {
+function getVendedorById (id) {
   return fetch(`${URL}/api/vendedor/${id}`).then(res => res.json());
 }
 
-async function getLastNumeroFactura () {
+function getLastNumeroFactura () {
   return fetch(`${URL}/api/factura/last`).then(res => res.json());
 }
 
-async function getTurnoActual () {
+function getTurnoActual () {
   return fetch(`${URL}/api/turno/actual`).then(res => res.json());
 }
 
 export {
   getTable,
   getAllArticulos,
+  getAllFacturas,
   getTurnoActual,
   getArticuloByCodigo,
   getArticuloById,
