4ca3539537beb87667bcf2613672b095dace5f91
Christopher Rieder <christopher.rieder@gmail.com>
2019-02-28 21:34:16 -0300

    .

--- src/js/constants/bussinessConstants.js
+++ src/js/constants/bussinessConstants.js
@@ -1,12 +1,5 @@
-import * as databaseRead from '../database/getData';
-
-async function getTiposDePago () {
-  return databaseRead.getTable('TIPO_PAGO');
-}
-
 const descuentoMax = 80; // this would be 80%, or 0.8;
 
 export {
-  getTiposDePago,
   descuentoMax
 };
