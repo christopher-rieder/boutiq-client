a6952807a1a362cf796a008fa9fa3694af8bb193
Christopher Rieder <christopher.rieder@gmail.com>
2019-03-08 02:04:28 -0300

    using reducer in AgregarPago

--- src/js/venta/AgregarPago.js
+++ src/js/venta/AgregarPago.js
@@ -1,34 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import { InputSelect, InputTextField, useFormInputFloat } from '../components/inputs';
+import React, { useReducer } from 'react';
+import { InputSelect, InputTextField, InputFloatField } from '../components/inputs';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'setMonto':
+      return {
+        ...state,
+        monto: action.payload
+      };
+    case 'setTipoPago':
+      const tipoPago = action.payload;
+      const estadoPago = tipoPago.LISTA_DE_PRECIO === 'PRECIO_CONTADO'
+        ? {id: 1, NOMBRE: 'PAGADO'}
+        : {id: 2, NOMBRE: 'PENDIENTE'};
+      return {
+        ...state,
+        tipoPago,
+        estadoPago
+      };
+    default: return state;
+  }
+};
 
 export default function AgregarPago (props) {
-  const [tipoPago, setTipoPago] = useState({});
-  const [estadoPago, setEstadoPago] = useState({});
-  const [monto, /* */ , montoProps] = useFormInputFloat(0);
+  const [pago, dispatchPago] = useReducer(reducer, {tipoPago: {id: 0, NOMBRE: ''}, monto: 0, estadoPago: {id: 0, NOMBRE: ''}});
 
   function handleSubmit () {
-    if (tipoPago && monto > 0 && estadoPago) {
+    if (pago.tipoPago && pago.monto > 0 && pago.estadoPago) {
       props.handleSelection({
-        TIPO_PAGO: tipoPago,
-        MONTO: parseFloat(monto),
-        ESTADO: estadoPago
+        TIPO_PAGO: pago.tipoPago,
+        MONTO: parseFloat(pago.monto),
+        ESTADO: pago.estadoPago
       });
     }
     if (props.setDisplayModal) props.setDisplayModal(false);
   }
 
-  useEffect(() => {
-    switch (tipoPago.id) {
-      case 1: setEstadoPago({id: 1, NOMBRE: 'PAGADO'}); break;
-      default: setEstadoPago({id: 2, NOMBRE: 'PENDIENTE'}); break;
-    }
-  }, [tipoPago]);
-
   return (
     <div>
-      <InputSelect table='TIPO_PAGO' name='Tipos de pago' accessor='NOMBRE' value={tipoPago} setValue={setTipoPago} />
-      <InputTextField name='Monto' {...montoProps} autoComplete='off' />
-      <InputTextField name='Estado' value={estadoPago.NOMBRE} readOnly autoComplete='off' />
+      <InputSelect table='TIPO_PAGO' name='Tipos de pago' accessor='NOMBRE' value={pago.tipoPago} setValue={tipoPago => dispatchPago({type: 'setTipoPago', payload: tipoPago})} />
+      <InputFloatField name='Descuento' value={pago.monto} setValue={monto => dispatchPago({type: 'setMonto', payload: monto})} autoComplete='off' />
+      <InputTextField name='Estado' value={pago.estadoPago.NOMBRE} readOnly autoComplete='off' />
       <button className='codigo-search' onClick={handleSubmit}>AGREGAR PAGO</button>
     </div>
   );
