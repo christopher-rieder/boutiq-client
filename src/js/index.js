6407130ab087e1faf0c021f00b1c0f02f78d5b22
Christopher Rieder <christopher.rieder@gmail.com>
2019-03-13 20:16:05 -0300

    new main menu

--- src/js/index.js
+++ src/js/index.js
@@ -54,40 +54,65 @@ function App () {
               </div>
             </li>
             <li className='navigation__item'>
-              <button className='navigation__btn' onClick={() => setMainElement(<Venta />)} >VENTA</button>
+              <button className='navigation__btn' >VENTA</button>
+              <div className='navigation__crud-tables'>
+                <button className='navigation__btn' onClick={() => setMainElement(<Venta />)}>
+                  Realizar Venta
+                </button>
+                <button className='navigation__btn' onClick={() => setMainElement(<ConsultaFactura />)}>
+                  Consultar Ventas
+                </button>
+              </div>
             </li>
             <li className='navigation__item'>
-              <button className='navigation__btn' onClick={() => setMainElement(<Compra />)} >COMPRA</button>
+              <button className='navigation__btn' >COMPRA</button>
+              <div className='navigation__crud-tables'>
+                <button className='navigation__btn' onClick={() => setMainElement(<Compra />)}>
+                  Realizar Compra
+                </button>
+                <button className='navigation__btn' onClick={() => setMainElement(<ConsultaCompra />)}>
+                  Consultar Compras
+                </button>
+              </div>
             </li>
             <li className='navigation__item'>
-              <button className='navigation__btn' onClick={() => setMainElement(<ConsultaArticulo handleSelection={({id}) => setMainElement(<CrudArticulo initialState={{id}} />) /* TODO: ARTICULO CRUD */} />)}>ARTICULO</button>
+              <button className='navigation__btn' >ARTICULO</button>
+              <div className='navigation__crud-tables'>
+                <button className='navigation__btn' onClick={() => setMainElement(<ConsultaArticulo handleSelection={({id}) => setMainElement(<CrudArticulo initialState={{id}} />) /* TODO: ARTICULO CRUD */} />)}>
+                  Consultar Articulos
+                </button>
+                <button className='navigation__btn' onClick={() => setMainElement(<CrudArticulo />)}>
+                  Modificar Articulos
+                </button>
+                <button className='navigation__btn' onClick={() => setMainElement(<RealizarStock />)}>
+                  Realizar Stock (NO TERMINADO)
+                </button>
+              </div>
             </li>
             <li className='navigation__item'>
-              <button className='navigation__btn' onClick={() => setMainElement(<ConsultaFactura />)}>FACTURA</button>
+              <button className='navigation__btn' onClick={() => setMainElement(<ConsultaPagos />)}>PAGOS</button>
             </li>
             <li className='navigation__item'>
-              <button className='navigation__btn' onClick={() => setMainElement(<ConsultaCompra />)}>COMPRA</button>
+              <button className='navigation__btn' >SEÑAS</button>
+              <div className='navigation__crud-tables'>
+                <button className='navigation__btn' onClick={() => setMainElement(<Seña />)}>
+                  Realizar Seña
+                </button>
+                <button className='navigation__btn' onClick={() => setMainElement(<ConsultaSeña />)}>
+                  Consultar Señas
+                </button>
+              </div>
             </li>
             <li className='navigation__item'>
-              <button className='navigation__btn' onClick={() => setMainElement(<CrudArticulo />)}>CRUDARTICULO</button>
-            </li>
-            <li className='navigation__item'>
-              <button className='navigation__btn' onClick={() => setMainElement(<RealizarStock />)}>REALIZAR STOCK (WIP)</button>
-            </li>
-            <li className='navigation__item'>
-              <button className='navigation__btn' onClick={() => setMainElement(<ConsultaPagos />)}>VER PAGOS</button>
-            </li>
-            <li className='navigation__item'>
-              <button className='navigation__btn' onClick={() => setMainElement(<Seña />)}>SEÑA</button>
-            </li>
-            <li className='navigation__item'>
-              <button className='navigation__btn' onClick={() => setMainElement(<ConsultaSeña />)}>CONSULTA SEÑA</button>
-            </li>
-            <li className='navigation__item'>
-              <button className='navigation__btn' onClick={() => setMainElement(<Retiro />)}>RETIRO</button>
-            </li>
-            <li className='navigation__item'>
-              <button className='navigation__btn' onClick={() => setMainElement(<ConsultaRetiro />)}>CONSULTA RETIRO</button>
+              <button className='navigation__btn' >RETIROS</button>
+              <div className='navigation__crud-tables'>
+                <button className='navigation__btn' onClick={() => setMainElement(<Retiro />)}>
+                  Realizar Retiro
+                </button>
+                <button className='navigation__btn' onClick={() => setMainElement(<ConsultaRetiro />)}>
+                  Consultar Retiros
+                </button>
+              </div>
             </li>
           </ul>
         </nav>
