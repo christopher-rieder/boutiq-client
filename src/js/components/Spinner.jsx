74c42312aefc4f9d7d0d376a0602a56a4226b863
Christopher Rieder <christopher.rieder@gmail.com>
2019-03-24 15:55:29 -0300

    spinner update

--- src/js/components/Spinner.jsx
+++ src/js/components/Spinner.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import { css } from '@emotion/core';
-// First way to import
-import { ClipLoader } from 'react-spinners';
-
-const override = css`
-    display: block;
-    margin: auto auto;
-    border-color: red;
-`;
-
 export default function Spinner (props) {
   return (
-    <div className='loading'>
-      <ClipLoader
-        css={override}
-        sizeUnit={'px'}
-        size={150}
-        color={'#123abc'}
-      />
+    <div class='bouncing-loader'>
+      <div />
+      <div />
+      <div />
     </div>
   );
 }
